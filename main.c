3e9499d45314ac98f0fed0d67d171d1646665300
ResRipper <16681844+ResRipper@users.noreply.github.com>
2018-05-19 11:34:36 +0800

    get user's input

--- main.c
+++ main.c
@@ -1,9 +1,42 @@
 #include <stdio.h>
-#include <math.h>
 #include "./lib/f_lib.h"
+
+double V0;
+int angle;
 
 int main()
 {
+    printf("Calculate of The Ballistic Impact\n");
+    printf("    /V0\n");
+    printf("   /\n");
+    printf("  / \n");
+    printf(" / ·angle\n");
+    printf("/————————————\n\n");
+
+    
+    printf("Please input the V0\n");
+    scanf("%lf",&V0);
+    printf("Please input the angle\n");
+    scanf("%d",&angle);
+
+    int x,y;
+    printf("What do you want?\n1.Height and distance 2.time 3.both");
+    scanf("%d", &x);
+    if(x != 1 && x != 2 && x != 3)
+    {
+        printf("Sorry, you are on the wrong way.\n");
+        return 0;
+    }
+    if(x == 1 || x == 3)
+    {
+        printf("Do you need the data stream?\n1.Yes 2.No\n");
+        scanf("%d",&y);
+        if(y != 1 && y != 2)
+        {
+            printf("Sorry, you are on the wrong way.\n");
+            return 0;
+        }
+    }
 
     return 0;
 }

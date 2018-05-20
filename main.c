c34431ad29afc593076644265a2c3ed935dd9e04
ResRipper <16681844+ResRipper@users.noreply.github.com>
2018-05-20 19:58:58 +0800

    upload main.c and time()

--- main.c
+++ main.c
@@ -13,7 +13,7 @@ int main()
     printf(" / ·angle\n");
     printf("/————————————\n\n");
 
-    
+
     printf("Please input the V0\n");
     scanf("%lf",&V0);
     printf("Please input the angle\n");
@@ -22,21 +22,53 @@ int main()
     int x,y;
     printf("What do you want?\n1.Height and distance 2.time 3.both");
     scanf("%d", &x);
-    if(x != 1 && x != 2 && x != 3)
-    {
-        printf("Sorry, you are on the wrong way.\n");
-        return 0;
-    }
     if(x == 1 || x == 3)
     {
         printf("Do you need the data stream?\n1.Yes 2.No\n");
         scanf("%d",&y);
-        if(y != 1 && y != 2)
-        {
-            printf("Sorry, you are on the wrong way.\n");
-            return 0;
-        }
     }
 
+    switch(x)
+    {
+        case    1:
+            switch(y)
+            {
+                case 1:
+                    hei_dis(V0, angle, 1);
+                    break;
+
+                case 2:
+                    hei_dis(V0, angle, 0);
+                    break;
+
+                default:
+                    printf("Wrong choice, please run this program again.");
+            }
+            break;
+
+        case    2:
+            time(V0, angle);
+            break;
+
+        case    3:
+            switch(y)
+            {
+                case 1:
+                    hei_dis(V0, angle, 1);
+                    time(V0, angle);
+                    break;
+
+                case 2:
+                    hei_dis(V0, angle, 0);
+                    time(V0, angle);
+                    break;
+
+                default:
+                    printf("Wrong choice, please run this program again.");
+            }
+
+        default:
+            printf("Wrong choice, please run this program again.");
+    }
     return 0;
 }

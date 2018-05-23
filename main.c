ed06c8f53490f5689d3f3def993991cc1993ab47
ResRipper <16681844+ResRipper@users.noreply.github.com>
2018-05-23 18:27:39 +0800

    :new_moon_with_face:
    Rename main2.c to main.c
    change forxxx and THR to C file

--- main.c
+++ main.c
@@ -2,8 +2,28 @@
 #include <math.h>
 #include "./lib/f_lib.h"
 
+int angle,print;
+double v0;
+
 int main()
 {
+    printf("If you want the result of the height and distance, please input a 1. \n");
+    printf("___________________________________\n");
+    printf("If you want to see the corresponding points of the parabola please press 2\n ");
+    printf("___________________________________\n");
+    printf("Your choice is:\n ");
+    scanf("%d",&print);
+    printf("Enter v0 & Angle: \n");
+    printf("%lf:\n",v0);
+    sacnf("%lf",&v0);
+    printf("%d:",angle);
+    scanf("%d",&angle);
+    
+
+    hei_dis(v0,angle,print);
+
+    time(v0,angle);
+
 
     return 0;
 }
